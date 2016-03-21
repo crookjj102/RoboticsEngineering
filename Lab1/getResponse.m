f6db7ed35777810788aa2caaf2caf69283e4292a
Joshua Crook <Joshua Crook>
2016-03-21 07:01:22 -0700

    Test commit on new OS.

--- Lab1/getResponse.m
+++
@@ -1,8 +0,0 @@
-function response = getResponse(s)
-    warning off all
-    response = char(fread(s,1));
-    while((isempty(response))||(~strcmp(response(end),char(get(s,'Terminator')))))
-        response = [response,char(fread(s,1))];
-    end
-    warning on all
-end
